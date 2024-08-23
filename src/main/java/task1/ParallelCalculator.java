ffc52a16c4b3f82894cb548e14e611729245afdb
kate-lvlup <kate.yalanskaya@gmail.com>
2024-08-23 10:36:39 +0300

    add task1 to perform computational operations in a parallel stream

--- src/main/java/task1/ParallelCalculator.java
+++ src/main/java/task1/ParallelCalculator.java
@@ -2,4 +2,21 @@ package task1;
 
 import java.util.function.BinaryOperator;
 
-// Write your code here
+public class ParallelCalculator implements Runnable {
+
+    public int result;
+    private BinaryOperator<Integer> operation;
+    private int operand1;
+    private int operand2;
+
+    public ParallelCalculator(BinaryOperator<Integer> operation, int operand1, int operand2) {
+        this.operation = operation;
+        this.operand1 = operand1;
+        this.operand2 = operand2;
+    }
+
+    @Override
+    public void run() {
+        result = operation.apply(operand1, operand2);
+    }
+}
